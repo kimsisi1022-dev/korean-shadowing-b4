085bb6a875b74fe7cecb1a61be73c51ab8ba7a98
kimsisi1022-dev <kimsisi1022@gmail.com>
2026-07-23 06:21:22 +0200

    Add files via upload

---
+++ sentences.py
@@ -0,0 +1,18 @@
+sentence_level1 = [
+"Sorry 죄송합니다 [jesonghamnida]",
+"The game will be paused for a moment 게임 잠시 중단됩니다 [Game jamsi jungdan demnida]"
+"I will proceed again after verifying this 확인 후, 다시 진행하겠습니다 [hwagin hu, dasi jinhaeng hagetseumnida]"
+"I am waiting for assistance 도움을 기다리고 있습니다 [doumeul gidarigo itsseum nida]"
+"Thank you for waiting so long 오래 기다려 주셔서 감사합니다 [orae gidaryeo jusyeoseo gamsahamnida]
+"The game is starting again 게임 다시 시작합니다 [Game dasi sijakamnida]"
+"Hello dear Players 플레이어 여러분 안녕하세요 [Player yeoreobun annyeonghaseyo]"
+"I am dealer OO your for this session 이번 세션을 맡은 딜러 OO입니다 [ibeon sessioneul mateun
+dealer (your name) imnida]"
+"I am dealer OO your for this session 이번 세션을 맡은 딜러 OO입니다 [ibeon session dealer (your name) imnida]"
+"Betting is closed, And the game is about to begin 베팅 마감하고 게임 시작합니다 [betting magamhago game sijakamnida]"
+"Wishing you good luck 행운을 빌어요 [Haenguneul bireoyo]"
+"It’s time to place your bets. Please place your bets 베팅 시간입니다. 베팅해 주세요 [betting sigan imnida. betting hae juseyo.]
+"Betting time is open 베팅 시간 오픈 되었습니다 [betting sigan open de eot sseum nida ]"
+"The new round is open. Please place your bets. 새 라운드가 오픈되었습니다. 베팅해 주세요. [sae round-ga open de eot seumnida. betting hae juseyo.]"
+"The new round is open. Please place your bets. 새로운 라운드가 오픈되었습니다. 베팅해 주세요. [saeroun round-ga open de eot seumnida. betting hae juseyo.]"
+]
